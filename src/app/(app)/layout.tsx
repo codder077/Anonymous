8e35dbac558a42a0535fed0f058934f41f142d62
codder077 <120477792+codder077@users.noreply.github.com>
2024-07-05 14:40:52 +0530

    final commit

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/toaster"
+// import "./../globals.css";
+
 import Navbar from "@/components/navbar";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,14 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
+
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar />
         {children}
-      <Toaster/>
+
       </body>
-      
-      </AuthProvider>
+
+
     </html>
   );
 }
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,136 @@
-import React from 'react';
+"use client";
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useToast } from "@/components/ui/use-toast";
+import {  Loader2 } from "lucide-react";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { signInSchema } from "@/schemas/signInSchema";
+import { signIn } from "next-auth/react";
 
-export default function page() {
-  return (
-    <div>
-      pages
-    </div>
-  );
+
+
+function page() {
+
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+
+    const { toast } = useToast();
+    const router = useRouter()
+    const form = useForm<z.infer<typeof signInSchema>>({
+        resolver: zodResolver(signInSchema),
+        defaultValues: {
+
+            identifier: "",
+            password: "",
+        },
+    });
+
+
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+        const result = await signIn('credentials', {
+            redirect: false,
+            identifier: data.identifier,
+            password: data.password,
+        })
+
+        if (result?.error) {
+            if (result?.error == "CredentialsSignin") {
+                toast: ({
+                    title: "Login failed",
+                    description: " Incorrect username or password",
+                    variant: "destructive"
+                })
+            } else {
+                toast: ({
+                    title: "Error",
+                    description: result.error,
+                    variant: "destructive"
+                })
+            }
+        }
+        if (result?.url) {
+            router.replace("/dashboard")
+        }
+    }
+
+
+
+    return (
+
+        <div className="flex justify-center item-center min-h-screen bg-gray-100 p-8">
+            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+                <div className="text-center">
+                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+                        Join Mystery Message
+                    </h1>
+                    <p className="mb-4">
+                        Sign in to start your anonymous adventure
+                    </p>
+                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+
+                        <FormField
+                            control={form.control}
+                            name="identifier"
+                            render={({ field }) => (
+                                <FormItem >
+                                    <FormLabel>Email/Username</FormLabel>
+                                    <FormControl>
+                                        <Input
+
+                                            placeholder="email/username"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name="password"
+                            control={form.control}
+
+                            render={({ field }) => (
+                                <FormItem >
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" placeholder="password"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit" disabled={isSubmitting} >
+                            {
+                                isSubmitting ? (
+                                    <>
+                                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                                        Please wait
+                                    </>
+                                ) : ('Signin')
+                            }
+                        </Button>
+                    </form>
+                </Form>
+                <p>
+                    Already a member?{' '}
+                    <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+                        Sign up
+                    </Link>
+                </p>
+            </div>
+        </div>
+    )
 }
+
+
+export default page;
